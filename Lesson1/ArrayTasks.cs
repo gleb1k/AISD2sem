60b25af7ce3c796dbc7a7a01211e2922a8dd4710
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-04-17 22:28:36 +0300

    сорт слиянием

--- Lesson1/ArrayTasks.cs
+++ Lesson1/ArrayTasks.cs
@@ -247,5 +247,75 @@ namespace Lesson1
 
             Array.Resize<int>(ref array, array.Length - 1);
         }
+        /// <summary>
+        /// Максимальный элемент в массиве методом разделяй и властвуй. Дз до 21.04.22
+        /// </summary>
+        /// <returns></returns>
+        public static int MaxElem()
+        {
+            return 0;
+        }
+        /// <summary>
+        /// Сортировка слиянием
+        /// </summary>
+        /// <param name="array"></param>
+        public static void MergeSort(int[] array)
+        {
+            if (array.Length == 1)
+                return;
+            int mid = array.Length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[array.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+            {
+                left[i] = array[i];
+            }
+            for (int i = mid; i < array.Length; i++)
+            {
+                right[i - mid] = array[i];
+            }
+
+            MergeSort(left);
+            MergeSort(right);
+            Merge(array, left, right);
+        }
+        private static void Merge(int[] targetArray, int[] array1, int[] array2)
+        {
+            int array1MinIndex = 0;
+            int array2MinIndex = 0;
+
+            int targetArrayMinIndex = 0;
+
+            //заполняем пока не кончится один из массивов
+            while (array1MinIndex < array1.Length && array2MinIndex < array2.Length)
+            {
+                if (array1[array1MinIndex] < array2[array2MinIndex])
+                {
+                    targetArray[targetArrayMinIndex] = array1[array1MinIndex];
+                    array1MinIndex++;
+                }
+                else
+                {
+                    targetArray[targetArrayMinIndex] = array2[array2MinIndex];
+                    array2MinIndex++;
+                }
+                targetArrayMinIndex++;
+            }
+            //дозаполняем оставшимся
+            while (array1MinIndex < array1.Length)
+            {
+                targetArray[targetArrayMinIndex] = array1[array1MinIndex];
+                array1MinIndex++;
+                targetArrayMinIndex++;
+            }
+            while (array2MinIndex < array2.Length)
+            {
+                targetArray[targetArrayMinIndex] = array2[array2MinIndex];
+                array2MinIndex++;
+                targetArrayMinIndex++;
+            }
+        }
+
     }
 }
